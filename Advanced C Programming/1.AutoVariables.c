1f67ed1b3aa147555379ef3c90d3c7a4b323034e
foobar <seebanger@gmail.com>
2021-07-15 13:40:33 +0000

    Added a very simple program

---
+++ Advanced C Programming/1.AutoVariables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int
+main ()
+{
+  auto int x; // Local varibale
+  x = 0;
+
+  return x;
+}
+
+int
+function ()
+{
+  auto int y; // Local variable
+  y = 0;
+
+  return y;
+}
